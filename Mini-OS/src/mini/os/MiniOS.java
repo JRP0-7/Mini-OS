cce2d3a62f82c16c0691992ab02643e5903664ef
JoseRafael <jose_rafael2@unitec.edu>
2026-08-30 18:26:57 -0600

    Hardcodear usuario prueba

--- Mini-OS/src/mini/os/MiniOS.java
+++ Mini-OS/src/mini/os/MiniOS.java
@@ -1,5 +1,6 @@
 package mini.os;
 
+import mini.os.core.GestorUser;
 import mini.os.core.Sistema;
 import mini.os.ui.VentanaMain;
 
@@ -8,11 +9,15 @@ public class MiniOS {
     public static void main(String[] args) {
         Sistema.iniciar();
 
+        try {
+            GestorUser.crearUser("alex", "1234");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         VentanaMain ventana = new VentanaMain();
         ventana.setVisible(true);
 
-
-        
     }
 
 }
